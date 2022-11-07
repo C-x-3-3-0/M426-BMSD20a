4e1c6e9b3b90145cf128bfb65f9815260162260d
Hao-Wang <89629020+C-x-3-3-0@users.noreply.github.com>
2022-11-07 18:46:04 +0100

    first part of TDD task, setting the rules

--- Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
+++ Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
@@ -1,12 +1,54 @@
 using Xunit;
 
+
 namespace FooBarQix.Tests
 {
     public class FooBarQixDeterminerTest
     {
         [Fact]
-        public void Test1()
+        public void Determine_ReturnsNumberAsString()
         {
+            int number = 8;
+
+            var testee = new FooBarQixDeterminer();
+
+            var result = testee.Determine(number);
+
+            Assert.Equal(number.ToString(), result);
         }
-    }
-}
+
+        [Fact]
+        public void Determine_ReturnsFoo()
+        {
+            var number = 3;
+
+            var testee = new FooBarQixDeterminer();
+
+            var result = testee.Determine(number);
+
+            Assert.Equal("Foo", result);
+        }
+
+        [Fact]
+        public void Determine_ReturnsBar()
+        {
+            int number = 5;
+
+            var testee = new FooBarQixDeterminer();
+
+            var result = testee.Determine(number);
+
+            Assert.Equal("Bar", result);
+        }
+
+        [Fact]
+        public void Determine_ReturnsQix()
+        {
+            int number = 7;
+
+            var testee = new FooBarQixDeterminer();
+
+            var result = testee.Determine(number);
+
+            Assert.Equal("Qix", result);
+        }
